83f86f2471db88c8b6c0a40f73a21c6adf30d486
jensmeichler <50821093+jensmeichler@users.noreply.github.com>
2024-07-19 10:59:04 +0000

    Deploying to prod from @ jensmeichler/tee-timers@83bb673ef0af0cb54d0e073ed28d4c939097c585 🚀

--- src/app/config/contentful-client.config.ts
+++
@@ -1,6 +0,0 @@
-import {CreateClientParams} from 'contentful/dist/types/contentful';
-
-export const CONTENTFUL_CONFIG: CreateClientParams = {
-  space: 'your_space_id',
-  accessToken: 'your_access_token'
-}
--- src/app/models/page.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Page {
-  title: string;
-  description?: string;
-  route: string;
-  content?: any;
-}
