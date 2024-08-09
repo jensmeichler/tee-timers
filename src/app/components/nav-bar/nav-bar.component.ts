214bd352b47585e8361d66e56153c6256e0483ba
Jens Meichler <j.meichler@rebuy.com>
2024-08-09 12:39:58 +0200

    feat: homepage nav menu

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,8 +1,8 @@
 import {Component, inject, Input} from '@angular/core';
 import {Page} from '../../models/page.model';
 import {AsyncPipe} from '@angular/common';
-import {Router, RouterLink, RouterLinkActive} from '@angular/router';
-import {map} from "rxjs";
+import {NavigationEnd, Router, RouterLink, RouterLinkActive} from '@angular/router';
+import {filter, map, startWith} from "rxjs";
 
 @Component({
   selector: 'tt-nav-bar',
@@ -14,7 +14,11 @@ import {map} from "rxjs";
 export class NavBarComponent {
   @Input() nodes: Page[] = [];
 
-  protected readonly isHomePage$ = inject(Router).events.pipe(map(() => location.pathname === '/'));
+  protected readonly isHomePage$ = inject(Router).events.pipe(
+    filter(event => event instanceof NavigationEnd),
+    map(() => location.pathname === '/'),
+    startWith(location.pathname === '/'),
+  );
 
   protected sortNodes(nodes: Page[]): Page[] {
     return nodes.sort((a, b) => (a.menuIndex || 0) - (b.menuIndex || 0));
